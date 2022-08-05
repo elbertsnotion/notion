4ac773bed8efd9ae14f41a9a3d4930837b5add16
ekim420 <ekim420@bloomberg.net>
2022-08-05 18:11:58 -0400

    add jsx intrinsic element

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button, TextField } from '@material-ui/core';
 
 function App() {
-
-  function clickHandler() {
-    console.log("got up to here");
-  }
 
   return (
 
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Came this far.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p></p>
-
-        {/* Button Component */}
-        <Button href="#" onClick={() => {const name = 'Elbert'; alert('Hello ', name);}} variant="contained">Click me</Button>
-
+        <input type="text" className="input" value="default"></input>
       </header>
+      <div className="articles">
+
+      </div>
     </div>
   );
 }
