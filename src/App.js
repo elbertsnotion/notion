e9c996227ecb86182d7c24b1898e19c9b59ad593
ekim420 <ekim420@bloomberg.net>
2022-08-05 18:39:14 -0400

    console.log the api response

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('webdev')
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/webdev.json").then(res => {
+      if (res.status != 200) {
+        console.log("ERROR!")
+        return;
+      }
+
+      res.json().then(data => {
+        if (data != null) {
+          console.log(data);
+        }
+      });
+    })
+  }, [subreddit]);
 
   return (
 
