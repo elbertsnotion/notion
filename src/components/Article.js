2156d495e87f4338da7a4673efbece279ce4afc3
ekim420 <ekim420@bloomberg.net>
2022-08-11 17:32:11 -0400

    add collapsible component and display # of comments inline

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Comments from './Comments';
+import Collapsible from 'react-collapsible';
 
 function Article(props) {
+
+    const [childData, setChildData] = useState("test");
+
     return (
         <article>
             <a href={ "https://reddit.com" + props.article.permalink } target="_blank" >
                 <h3>{ props.article.title }</h3>
-                <Comments link={ props.article.permalink }/>
             </a>
+            {/* <p>{ childData }</p> */}
+
+            <Collapsible trigger={childData}>
+                <Comments link={ props.article.permalink } passChildData={ setChildData }/>
+            </Collapsible>
         </article>
     );
 }
--- src/components/Comments.js
+++ src/components/Comments.js
@@ -2,11 +2,17 @@ import React, {useState, useEffect} from 'react';
 
 function Comments(props) {
 
+    const functionHandler = (data) => {
+      data = data + " comments";
+      props.passChildData(data);
+    }
+
     const [comments, setComments] = useState([]);
     let c;
     let c1 = [];
 
     const commentsLink = props.link.substring(0, props.link.length - 1);
+    functionHandler(comments.length);
 
     useEffect(() => {
       fetch("https://www.reddit.com" + commentsLink + ".json").then(res => {
@@ -31,32 +37,11 @@ function Comments(props) {
             }
 
             setComments(c1);
-
-            // loop through and ignore those with length 0
-            // if (c.length > 0){
-            //   for (var i = 0; i < c.length; i++) {
-            //     // console.log(c[i]);
-
-            //     if (c[i].hasOwnProperty('data')) {
-            //       if (c[i]['data'].hasOwnProperty('body')){
-            //         setComments(comments => [...comments, ...c[i]['data']['body']]);
-            //         console.log(comments);
-            //       } else {
-            //         console.log(c[i]['data']);
-            //       }
-            //       // setComments(comments => [...comments, c[i]['data']['body']]);
-
-            //     }
-            //     // console.log(c[i]['data']['body']);
-            //     // if (c[i]['data']){
-            //     //   setComments(comments => [...comments, c[i]['data']['body']]);
-            //     // }
-
-            //   }
-            // }
+            // functionHandler(comments.length);
 
           }
         });
+
       })
 
     }, []);
@@ -64,7 +49,9 @@ function Comments(props) {
     return (
       <div className="comments">
         {
-        (comments != null) ? comments.map( (comment, index) => <p key={index}>{comment}</p>) : ''
+        (comments != null) ? comments.map( (comment, index) =>
+
+        <div><p key={index}>{comment}</p><br></br></div>) : ''
         }
       </div>
     );
